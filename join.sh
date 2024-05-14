47a0e88cc4b6a6327745a436572a2513405c89d8
HayZun <paul.durieux@humansconnexion.com>
2024-05-14 16:10:41 +0200

    update VARS and pre-prompt password

--- join.sh
+++ join.sh
@@ -4,30 +4,36 @@
 
 ##### PRE-REQUIS
 # - être connecté au wifi invite
+# - avoir les packages apt "sssd-ad sssd-tools realmd" installé
 
 ##### VARS
+
+# config
 DOMAIN="XXXXXX"
 DNS=""
-ADMIN=""
-OU=""
+
+# AD
+ADMIN_DCA=""
+PASSWORD_DCA=""
+OU_PATH_COMPUTERS=""
+
+## wifi
 SSID_WIFI_AD=""
 PASSWORD_WIFI_AD=""
-## installer package joinAD
-sudo apt install sssd-ad sssd-tools realmd
+
+# se connecter au wifi TEAM
+nmcli device wifi connect $SSID_WIFI password $PASSWORD_WIFI
 
 ## setup le DNS
 # stoper service systemd-resolved
-sudo systemctl stop systemd-resolved
+systemctl stop systemd-resolved
 
 # ecrire dans /etc/resolv.conf
 echo "search $DOMAIN" > /etc/resolv.conf
 echo "nameserver $DNS" >> /etc/resolv.conf
 
-# se connecter au wifi TEAM
-nmcli device wifi connect $SSID_WIFI_AD password $PASSWORD_WIFI_AD
-
 ## join AD
-sudo realm -v discover $DOMAIN
+realm -v discover $DOMAIN
 
-
-sudo realm join $DOMAIN --user=$ADMIN --computer-ou=$OU
+#
+echo $PASSWORD_DCA | realm join --user=$ADMIN_DCA --computer-ou=$OU_PATH_COMPUTERS $DOMAIN
