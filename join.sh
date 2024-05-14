d970f7cbd2e59fb911377844b0b8d7b9ddb1ba86
HayZun <paul.durieux@humansconnexion.com>
2024-05-14 15:50:28 +0200

    fix vars

--- join.sh
+++ join.sh
@@ -10,7 +10,8 @@ DOMAIN="XXXXXX"
 DNS=""
 ADMIN=""
 OU=""
-
+SSID_WIFI_AD=""
+PASSWORD_WIFI_AD=""
 ## installer package joinAD
 sudo apt install sssd-ad sssd-tools realmd
 
@@ -20,13 +21,13 @@ sudo systemctl stop systemd-resolved
 
 # ecrire dans /etc/resolv.conf
 echo "search $DOMAIN" > /etc/resolv.conf
-echo "nameserver 10.231.40.82" >> /etc/resolv.conf
+echo "nameserver $DNS" >> /etc/resolv.conf
 
 # se connecter au wifi TEAM
-nmcli device wifi connect TEAM password 'team2022'
+nmcli device wifi connect $SSID_WIFI_AD password $PASSWORD_WIFI_AD
 
 ## join AD
 sudo realm -v discover $DOMAIN
 
-#
-sudo realm join $DOMAIN --user=$ADMIN --computer-ou=$OU
+
+sudo realm join $DOMAIN --user=$ADMIN --computer-ou="OU=Linux,OU=Portables,OU=Ordinateurs,DC=humans,DC=local humans.local" --install=/
